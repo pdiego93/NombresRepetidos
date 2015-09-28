0a1771f99e659a38b5701651b1a8213887dc5fb6
pdiego93 <pdiego93@gmail.com>
2015-09-28 14:58:33 -0300

    Falta completar

---
+++ src/paquete/ContadorCargador.java
@@ -0,0 +1,68 @@
+package paquete;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class ContadorCargador {
+	int repes;
+	
+	public List<Nombre> obtenerNombres(String ruta){
+		
+		File archivo = null;
+		FileReader fr = null;
+		BufferedReader br = null;
+		Nombre aux;
+		List <Nombre> nom = new ArrayList <Nombre>();
+		
+		int tam=0;
+		
+		repes=0; //cantidad de mas repetidos a buscar
+		
+		try{
+			String linea;
+			archivo = new File(ruta);
+			fr = new FileReader(archivo);
+			br = new BufferedReader(fr);
+			
+			if ((linea = br.readLine()) != null){
+				String [] data = linea.split(" ");
+				tam = Integer.parseInt(data[0]);
+				repes = Integer.parseInt(data[1]);
+			}
+						
+			int contElem = 0,
+					i=0;
+			while (contElem < tam && (linea = br.readLine()) != null) {
+				if((i=nom.indexOf(linea))<0){
+					aux = new Nombre(linea);
+					nom.add(aux);
+				}
+				else
+					nom.get(i).sumar();	
+				contElem++;
+			}
+		}
+		 catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					if (fr != null)
+						fr.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+	}
+		return nom;
+}
+	
+	public List<Nombre> obtenerMaxRepetidos(){
+		
+	}
+	
+}
---
+++ src/paquete/Nombre.java
@@ -0,0 +1,32 @@
+package paquete;
+
+public class Nombre {
+	String nom;
+	int cont=0;
+	
+	public Nombre(String nombre){
+		nom = nombre;
+	}
+	
+	public String getNombre(){
+		return nom;
+	}
+	
+	public int getCant(){
+		return cont;
+	}
+	
+	public void setNombre(String nombre){
+		nom = nombre;
+	}
+
+	public void setCero(){
+		cont = 0;
+	}
+	
+	public void sumar(){
+		cont++;
+	}
+	
+	
+}
